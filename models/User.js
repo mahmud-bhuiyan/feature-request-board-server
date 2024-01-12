ff20a4914ff91500355b85e4cc8c70d29c180bc3
mahmud-bhuiyan <bh.mahmud@gmail.com>
2024-01-12 19:35:02 +0600

    default image added for user

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String },
-    photoURL: { type: String },
+    photoURL: { type: String, default: "https://i.ibb.co/0qwVw3b/p-2.jpg" },
     role: { type: String, default: "user" },
     isDeleted: { type: Boolean, default: false },
   },

addc006db8966cba5b46c23c7cb536100e7367ce
mahmud-bhuiyan <bh.mahmud@gmail.com>
2024-01-13 23:49:50 +0600

    update website info api created

--- controllers/websiteConfigController.js
+++ controllers/websiteConfigController.js
@@ -21,6 +21,37 @@ const getWebsiteInfo = asyncWrapper(async (req, res) => {
   });
 });
 
+/**
+ * update Website Info
+ * /api/v1/website/
+ * private route (patch)
+ */
+const updateWebsiteInfo = asyncWrapper(async (req, res) => {
+  // Assuming req.body contains the updated website information
+  const updatedWebsiteInfo = req.body;
+
+  // Find the existing website details in the database
+  let websiteDetails = await WebsiteConfig.findOne();
+
+  if (websiteDetails) {
+    // Update only the fields that are present in the request
+    Object.keys(updatedWebsiteInfo).forEach((key) => {
+      if (websiteDetails.schema.paths[key]) {
+        websiteDetails[key] = updatedWebsiteInfo[key];
+      }
+    });
+  }
+
+  // Save the updated/created website details
+  await websiteDetails.save();
+
+  res.status(200).json({
+    message: "Website info updated successfully",
+    websiteInfo: websiteDetails,
+  });
+});
+
 module.exports = {
   getWebsiteInfo,
+  updateWebsiteInfo,
 };
--- routes/websiteRoutes.js
+++ routes/websiteRoutes.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const auth = require("../middlewares/auth");
 const checkAdmin = require("../middlewares/checkAdmin");
 
-const { getWebsiteInfo } = require("../controllers/websiteConfigController");
+const {
+  getWebsiteInfo,
+  updateWebsiteInfo,
+} = require("../controllers/websiteConfigController");
 
-router.route("/").get(auth, checkAdmin, getWebsiteInfo);
+router
+  .route("/")
+  .get(auth, checkAdmin, getWebsiteInfo)
+  .patch(auth, checkAdmin, updateWebsiteInfo);
 
 module.exports = router;
